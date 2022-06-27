5586c802c5aeb1336913ae7523d1d76f0274db29
Evan Rosson <rossonevan7281@gmail.com>
2022-06-27 17:43:30 -0500

    commit tree a86ecd8fd78cb3acee624d50f6dbd55e37b8c982

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
 
-// Write your code here
+const num3 = 12;
+const num4 = 3;
+const mod = num3 / num4;
+
+const max = Math.max(5,10,15,20);
